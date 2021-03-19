b34b1dcf4422763460b7f6e7764efccbd7a59fba
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2021-03-19 15:02:40 -0300

    Acrescentado mensagem de boa tarde

--- C8 - Git/Program.cs
+++ C8 - Git/Program.cs
@@ -7,6 +7,7 @@ namespace C8___Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa tarde");
         }
     }
 }
